2b8c04630316b6ae585b4b4eee467e6b55733fc5
Revathi G <revathikrishna0323@gmail.com>
2022-08-30 18:28:37 +0530

    changes made in source code

--- src/main/java/com/vehicles/Bike.java
+++ src/main/java/com/vehicles/Bike.java
@@ -6,5 +6,40 @@
 
 package com.vehicles;
 
-public class Bike {
+public class Bike extends VehicleManufacturer implements Vehicle {
+    //create a no args constructor
+    public Bike() {
+    }
+
+    // create a parameterised constructor
+    public Bike(String vehicleName, String vehicleModelName, String vehicleType) {
+        super(vehicleName, vehicleModelName, vehicleType);
+    }
+
+    @Override
+    public String getManufacturerInformation() {
+        return toString();
+    }
+
+    @Override
+    public int maxSpeed(String vehicleType) {
+        if (getVehicleType().equals("sportsBike")) {
+            return 250;
+        } else if (getVehicleType().equals("cruiser")) {
+
+            return 170;
+        } else return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Bike{} " + super.toString();
+    }
 }
+
+
+
+
+
+
+
